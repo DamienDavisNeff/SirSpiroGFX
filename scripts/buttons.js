d0aea24306b4e6856d24136becf7043fdb64ea0e
Damien Davis-Neff <damien@thirdplacestudios.ga>
2022-12-19 23:18:11 -0500

    Commented JS & Optimized Button Toggle
    -Commented code to describe what I am on about
    -Button toggle actually toggles it rather than checking the status and then changing it
    - Changed class names to work with new systems
    - Made function names consistent
    
    -- hopefully this works, because its going to be deployed

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,42 +1,43 @@
-function toggleShadow(state,id) {
+// The state is the enabled status [true = enabled, false = disabled]
+function ToggleShadow(state,id) {
     for(let a = 0; a < id.length; a++) {
         if(state) document.getElementById(id[a]).classList.add("shadow");
         if(!state) document.getElementById(id[a]).classList.remove("shadow");
     }
 }
-
-function HideButton(state,id) {
-    if(state) {
-        document.getElementById(id).classList.add("hidden");
-        document.getElementById(id).classList.remove("unhid");
-    }
-    if(!state) {
-        document.getElementById(id).classList.add("unhid");
-        document.getElementById(id).classList.remove("hidden");
-    }
+// The state is the enabled status [true = enabled, false = disabled]
+function ToggleButton(state,id) {
+    document.getElementById(id).classList.add(state);
+    document.getElementById(id).classList.remove(!state);
 }
-function ChangeCursor(id,enabledType) {
+// The ID is an array of elements to change the cursor while hovering over
+function ChangeCursor(id,cursorType) {
     for(let a = 0; a < id.length; a++) {
-        document.getElementById(id[a]).style.cursor = enabledType;
+        document.getElementById(id[a]).style.cursor = cursorType;
     }
 }
 
+// Runs this function when scrolling on the page
 window.onscroll = function(e) {
-    if(this.scrollY >= window.innerHeight / 3) {
-        HideButton(false,"upButton","upButtonCircle","upButtonIcon");
+    if(this.scrollY >= window.innerHeight / 3) { // If the page is low enough
+        ToggleButton(true,"upButton");
         ChangeCursor(["upButtonCircle","upButtonIcon"],"pointer");
-    } else {
-        HideButton(true,"upButton","upButtonCircle","upButtonIcon");
+    } else { // Otherwise
+        ToggleButton(false,"upButton","upButtonCircle","upButtonIcon");
         ChangeCursor(["upButtonCircle","upButtonIcon"],"default");
     }
-    
 }
 
 function GoTo(link,id) {
-    if(document.getElementById(id).getAttribute("class").includes("hidden")) return;
+    if(document.getElementById(id).getAttribute("class").includes("hidden")) return; // If the button is disabled, does not proceed
+    /*
+    If a ID is included in the link, splits it, and adds the *last* id to the start of the link
+    This is because sometimes I use 'window.location.href' which includes the id that is already there
+    So it removes all IDs, except for the one that was 'manually' added
+    */
     if(link.includes("#")) {
         temp = link.split("#");
-        link = `${temp[0]}#${temp[temp.length-1]}`;
+        link = `${temp[0]}#${temp[temp.length-1]}`; // Takes the start of the link, and adds the last ID to it
     }
     window.open(link,"_self");
 }
