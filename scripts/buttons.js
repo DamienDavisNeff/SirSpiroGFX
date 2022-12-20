653033d785894168f9450508dcf3aaa03ca7d43e
Damien Davis-Neff <damien@thirdplacestudios.ga>
2022-12-19 22:26:35 -0500

    Fixed bugs
    - Rolled back previous "fix" for these actual fixes
    - Can no longer click up button while its hidden
    - Up button does not hide instantly
    
    -- Code for this is overly complex, although it will be fixed in next commit --

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -7,29 +7,35 @@ function removeShadow(id) {
     document.getElementById(id).classList.remove("shadow");
 }
 
-
-function HideButton(state,id) {
+function HideButton(state,id,id2,id3) {
     if(state) {
-        document.getElementById(id).classList.remove("unhidden");
         document.getElementById(id).classList.add("hidden");
+        document.getElementById(id).classList.remove("unhid");
+        document.getElementById(id2).style.cursor = "default";
+        document.getElementById(id3).style.cursor = "default";
     }
     if(!state) {
-        document.getElementById(id).classList.add("unhidden");
+        document.getElementById(id).classList.add("unhid");
         document.getElementById(id).classList.remove("hidden");
+        document.getElementById(id2).style.cursor = "pointer";
+        document.getElementById(id3).style.cursor = "pointer";
     }
-    UpdateState(id);
-}
-function UpdateState(id) {
-    setTimeout(() => {
-        if(document.getElementById("upButton").classList.contains("hidden")) {
-            document.getElementById("upButton").hidden = true;
-        } else { document.getElementById("upButton").hidden = false; }
-    }, 1000);
 }
 
 window.onscroll = function(e) {
-    HideButton(true,"upButton");
-    if(this.scrollY >= window.innerHeight / 3) HideButton(false,"upButton");
-
+    if(this.scrollY >= window.innerHeight / 3) {
+        HideButton(false,"upButton","upButtonCircle","upButtonIcon");
+    } else {
+        HideButton(true,"upButton","upButtonCircle","upButtonIcon");
+    }
     
+}
+
+function GoTo(link,id) {
+    if(document.getElementById(id).getAttribute("class").includes("hidden")) return;
+    if(link.includes("#")) {
+        temp = link.split("#");
+        link = `${temp[0]}#${temp[temp.length-1]}`;
+    }
+    window.open(link,"_self");
 }
