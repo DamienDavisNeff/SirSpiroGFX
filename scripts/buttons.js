ddf88738387b00ca5bb92e74d0ee494272172e64
Damien Davis-Neff <damien@thirdplacestudios.ga>
2022-12-20 09:45:22 -0500

    Moved some functions to global script
    Moved some functions that can be used globally to its own script. Optimized ToggleShadows function.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,22 +1,3 @@
-// The state is the enabled status [true = enabled, false = disabled]
-function ToggleShadow(state,id) {
-    for(let a = 0; a < id.length; a++) {
-        if(state) document.getElementById(id[a]).classList.add("shadow");
-        if(!state) document.getElementById(id[a]).classList.remove("shadow");
-    }
-}
-// The state is the enabled status [true = enabled, false = disabled]
-function ToggleButton(state,id) {
-    document.getElementById(id).classList.add(state);
-    document.getElementById(id).classList.remove(!state);
-}
-// The ID is an array of elements to change the cursor while hovering over
-function ChangeCursor(id,cursorType) {
-    for(let a = 0; a < id.length; a++) {
-        document.getElementById(id[a]).style.cursor = cursorType;
-    }
-}
-
 // Runs this function when scrolling on the page
 window.onscroll = function(e) {
     if(this.scrollY >= window.innerHeight / 3) { // If the page is low enough
